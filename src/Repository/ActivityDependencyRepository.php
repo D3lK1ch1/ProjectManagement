582edfa7069140ad5db36391964c6b41fdad1e28
D3lK1ch1 <dhizkch@gmail.com>
2026-04-29 15:45:33 +1000

    Clean up comments

--- src/Repository/ActivityDependencyRepository.php
+++ src/Repository/ActivityDependencyRepository.php
@@ -15,29 +15,4 @@ class ActivityDependencyRepository extends ServiceEntityRepository
     {
         parent::__construct($registry, ActivityDependency::class);
     }
-
-//    /**
-//     * @return ActivityDependency[] Returns an array of ActivityDependency objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('a.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?ActivityDependency
-//    {
-//        return $this->createQueryBuilder('a')
-//            ->andWhere('a.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
 }
