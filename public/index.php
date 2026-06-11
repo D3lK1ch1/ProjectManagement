51c145354ecf62e350eb862196c32f2abcd8c3bc
D3lK1ch1 <dhizkch@gmail.com>
2026-06-11 11:56:23 +1000

    Configuration with a static demo page for visualized project flow

--- public/index.php
+++ public/index.php
@@ -1,5 +1,13 @@
 <?php
 
+if (PHP_SAPI === 'cli-server') {
+    $path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    $file = __DIR__ . $path;
+    if (is_file($file) && !str_ends_with($file, '.php')) {
+        return false;
+    }
+}
+
 use App\Kernel;
 
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
